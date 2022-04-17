bcc7f5618ea592152cac28656412e03228bbae69
yanguojin <guojin_yjs@cumt.edu.cn>
2022-04-17 16:20:21 +0800

    添加项目文件。

---
+++ openvino/charp_openvino_yolov5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace charp_openvino_yolov5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ openvino/csharp_openvino_class/Core.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace OpenVinoSharp
+{
+    public class Core
+    {
+    }
+}
